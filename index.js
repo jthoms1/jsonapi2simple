a4f92d33543ec6172d3f675ad5150d9f3106424a
Josh Thomas <jthoms1@gmail.com>
2015-03-02 14:47:14 -0600

    Adding base resources to repo.

---
+++ index.js
@@ -0,0 +1,62 @@
+'use strict';
+
+let request = require('superagent');
+
+function promiseYouWill(req) {
+  return new Promise(function(resolve, reject) {
+    req.end(function(err, res) {
+      if (err) {
+        reject(err);
+      } if (!res.ok) {
+        reject(res.error);
+      }else {
+        resolve(res);
+      }
+    });
+  });
+}
+
+function _getResourceUrl(resourceName, id=null) {
+  let url = '/api/' + resourceName;
+  url += (id) ? '/' + id : '';
+  return url;
+}
+
+module.exports = {
+  get (resourceName, options={}) {
+    let resourceUrl = _getResourceUrl(resourceName);
+    let req = request
+      .get(resourceUrl)
+      .accept('application/json')
+      .query(options);
+
+    return promiseYouWill(req);
+  },
+
+  create (resourceName, resource, options={}) {
+    let resourceUrl = _getResourceUrl(resourceName, resource.id);
+    let req = request.post(resourceUrl)
+      .set('Content-Type', 'application/json')
+      .query(options)
+      .send(resource);
+
+    return promiseYouWill(req);
+  },
+
+  update (resourceName, resource, options={}) {
+    let resourceUrl = _getResourceUrl(resourceName, resource.id);
+    let req = request.put(resourceUrl)
+      .set('Content-Type', 'application/json')
+      .query(options)
+      .send(resource);
+
+    return promiseYouWill(req);
+  },
+
+  del (resourceName, resource) {
+    let resourceUrl = _getResourceUrl(resourceName, resource.id);
+    let req = request.del(resourceUrl);
+
+    return promiseYouWill(req);
+  }
+};
