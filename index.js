c650c794f12239a32ad7af7c214f48fe5cd0fcd4
Josh Thomas <jthoms1@gmail.com>
2015-03-02 18:18:01 -0600

    Fixed some bugs and began writing tests.

--- index.js
+++ index.js
@@ -1,62 +1,68 @@
 'use strict';
 
 var request = require('superagent');
+require('es6-promise').polyfill();
 
 function promiseYouWill(req) {
   return new Promise(function(resolve, reject) {
     req.end(function(err, res) {
       if (err) {
         reject(err);
-      } if (!res.ok) {
-        reject(res.error);
-      }else {
+      } if (res.ok) {
         resolve(res);
+      } else {
+        reject(res.error);
       }
     });
   });
 }
 
-function _getResourceUrl(resourceName, id=null) {
-  var url = '/api/' + resourceName;
-  url += (id) ? '/' + id : '';
-  return url;
-}
-
-module.exports = {
-  get: function(resourceName, options={}) {
-    var resourceUrl = _getResourceUrl(resourceName);
-    let req = request
-      .get(resourceUrl)
-      .accept('application/json')
-      .query(options);
-
-    return promiseYouWill(req);
-  },
-
-  create: function(resourceName, resource, options={}) {
-    var resourceUrl = _getResourceUrl(resourceName, resource.id);
-    var req = request.post(resourceUrl)
-      .set('Content-Type', 'application/json')
-      .query(options)
-      .send(resource);
-
-    return promiseYouWill(req);
-  },
-
-  update: function(resourceName, resource, options={}) {
-    var resourceUrl = _getResourceUrl(resourceName, resource.id);
-    var req = request.put(resourceUrl)
-      .set('Content-Type', 'application/json')
-      .query(options)
-      .send(resource);
-
-    return promiseYouWill(req);
-  },
-
-  del: function(resourceName, resource) {
-    var resourceUrl = _getResourceUrl(resourceName, resource.id);
-    var req = request.del(resourceUrl);
-
-    return promiseYouWill(req);
+module.exports = function(basePath) {
+  function _getResourceUrl(resourceName, id) {
+    var url = basePath + '/' + resourceName;
+    url += (id) ? '/' + id : '';
+    return url;
   }
+
+  return {
+    get: function(resourceName, options) {
+      options = options || {};
+      var resourceUrl = _getResourceUrl(resourceName);
+      var req = request
+        .get(resourceUrl)
+        .accept('application/json')
+        .query(options);
+
+      return promiseYouWill(req);
+    },
+
+    create: function(resourceName, resource, options) {
+      options = options || {};
+      var resourceUrl = _getResourceUrl(resourceName, resource.id);
+      var req = request.post(resourceUrl)
+        .set('Content-Type', 'application/json')
+        .query(options)
+        .send(resource);
+
+      return promiseYouWill(req);
+    },
+
+    update: function(resourceName, resource, options) {
+      options = options || {};
+      var resourceUrl = _getResourceUrl(resourceName, resource.id);
+      var req = request.put(resourceUrl)
+        .set('Content-Type', 'application/json')
+        .query(options)
+        .send(resource);
+
+      return promiseYouWill(req);
+    },
+
+    del: function(resourceName, resource) {
+      var resourceUrl = _getResourceUrl(resourceName, resource.id);
+      var req = request.del(resourceUrl);
+
+      return promiseYouWill(req);
+    }
+  };
 };
--- test/collection-get.js
+++ test/collection-get.js
@@ -4,19 +4,38 @@ var apiUtil = require('../index.js'),
   expect = require('expect.js'),
   sinon = require('sinon');
 
-describe('GET Collection', function () {
-  describe('Invalid Scenarios', function () {
-    it('', function (done) {
-      var server = sinon.fakeServer.create();
+var api = apiUtil('/api');
 
-      server.respondWith('GET', encodeURI('/api/things'), [404, {
-         'Content-Type': 'application/vnd.api+json'
+describe('GET Collection', function () {
+  var server;
+
+  before(function(done) {
+    server = sinon.fakeServer.create();
+    done();
+  });
+  after(function(done) {
+    server.restore();
+    done();
+  });
+
+  describe('Invalid Scenarios', function () {
+    it('it should throw a promise error on 400 level errors', function (done) {
+      server.respondWith('GET', '/api/things', [200, {
+          'Content-Type': 'application/json'
         }, JSON.stringify({
-          'errors': [{
-            'title': 'Resource not found.'
-          }]
+          'errors': 'blah'
         })
       ]);
+
+      api.get('things')
+        .then(function() {
+          console.log('fail');
+        })
+        .catch(function(err) {
+          console.log('hello ' + err);
+        });
+
+      server.respond();
 
       done();
     });
