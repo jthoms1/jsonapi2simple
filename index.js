29cebd2dfec01a21371ee5d0f938423ba1b2af3d
Josh Thomas <jthoms1@gmail.com>
2015-03-03 22:12:21 -0600

    Changed repo to be around transforming between json types.

--- index.js
+++ index.js
@@ -1,68 +1,10 @@
 'use strict';
+var assign = require('object-assign');
 
-var request = require('superagent');
-require('es6-promise').polyfill();
+exports.toSimple = function (data) {
+  return assign({}, data);
+};
 
-function promiseYouWill(req) {
-  return new Promise(function(resolve, reject) {
-    req.end(function(err, res) {
-      if (err) {
-        reject(err);
-      } if (res.ok) {
-        resolve(res);
-      } else {
-        reject(res.error);
-      }
-    });
-  });
-}
-
-module.exports = function(basePath) {
-  function _getResourceUrl(resourceName, id) {
-    var url = basePath + '/' + resourceName;
-    url += (id) ? '/' + id : '';
-    return url;
-  }
-
-  return {
-    get: function(resourceName, options) {
-      options = options || {};
-      var resourceUrl = _getResourceUrl(resourceName);
-      var req = request
-        .get(resourceUrl)
-        .accept('application/json')
-        .query(options);
-
-      return promiseYouWill(req);
-    },
-
-    create: function(resourceName, resource, options) {
-      options = options || {};
-      var resourceUrl = _getResourceUrl(resourceName, resource.id);
-      var req = request.post(resourceUrl)
-        .set('Content-Type', 'application/json')
-        .query(options)
-        .send(resource);
-
-      return promiseYouWill(req);
-    },
-
-    update: function(resourceName, resource, options) {
-      options = options || {};
-      var resourceUrl = _getResourceUrl(resourceName, resource.id);
-      var req = request.put(resourceUrl)
-        .set('Content-Type', 'application/json')
-        .query(options)
-        .send(resource);
-
-      return promiseYouWill(req);
-    },
-
-    del: function(resourceName, resource) {
-      var resourceUrl = _getResourceUrl(resourceName, resource.id);
-      var req = request.del(resourceUrl);
-
-      return promiseYouWill(req);
-    }
-  };
+exports.toJsonApi = function (data, info) {
+  return assign({}, data);
 };
---
+++ test/base.js
@@ -0,0 +1,238 @@
+'use strict';
+
+var transform = require('../index.js'),
+  expect = require('expect.js');
+
+describe('Data with no relationships', function () {
+  var info = {
+      'type': 'articles'
+  };
+  var apiData = {
+    'data': {
+      'type': 'articles',
+      'id': '1',
+      'title': 'Rails is a Melting Pot'
+    }
+  };
+  var simpleData = {
+    'id': '1',
+    'title': 'Rails is a Melting Pot'
+  };
+
+  it('should convert jsonapi data to simple', function (done) {
+    var result = transform.toSimple(apiData);
+    expect(result).to.eql(simpleData);
+    done();
+  });
+  it('should convert simple data to jsonapi', function (done) {
+    var result = transform.toJsonApi(simpleData, info);
+    expect(result).to.eql(apiData);
+    done();
+  });
+});
+
+describe('Data with 1-to-1 relationships', function () {
+  var info = {
+      'type': 'articles'
+  };
+  var apiData = {
+    'data': {
+      'type': 'articles',
+      'id': '1',
+      'title': 'Rails is a Melting Pot',
+      'links': {
+        'author': {
+          'type': 'people',
+           'id': '1'
+        }
+      }
+    }
+  };
+  var simpleData = {
+    'id': '1',
+    'title': 'Rails is a Melting Pot',
+    'authorId': '1'
+  };
+
+
+  it('should convert jsonapi data to simple', function (done) {
+    var result = transform.toSimple(apiData);
+    expect(result).to.eql(simpleData);
+    done();
+  });
+  it('should convert simple data to jsonapi', function (done) {
+    var result = transform.toJsonApi(simpleData, info);
+    expect(result).to.eql(apiData);
+    done();
+  });
+});
+
+describe('Data with 1-to-many relationships', function () {
+  var info = {
+      'type': 'articles'
+  };
+  var apiData = {
+    'data': {
+      'type': 'articles',
+      'id': '1',
+      'title': 'Rails is a Melting Pot',
+      'links': {
+        'tags': {
+          'type': 'tags',
+          'ids': ['2', '3']
+        }
+      }
+    }
+  };
+  var simpleData = {
+    'id': '1',
+    'title': 'Rails is a Melting Pot',
+    'tagIds': ['2', '3']
+  };
+
+  it('should convert jsonapi data to simple', function (done) {
+    var result = transform.toSimple(apiData);
+    expect(result).to.eql(simpleData);
+    done();
+  });
+  it('should convert simple data to jsonapi', function (done) {
+    var result = transform.toJsonApi(simpleData, info);
+    expect(result).to.eql(apiData);
+    done();
+  });
+});
+
+describe('Data with 1-to-many relationships', function () {
+  var info = {
+      'type': 'articles'
+  };
+  var apiData = {
+    'data': {
+      'type': 'articles',
+      'id': '1',
+      'title': 'Rails is a Melting Pot',
+      'links': {
+        'tags': {
+          'type': 'tags',
+          'ids': ['2', '3']
+        }
+      }
+    }
+  };
+  var simpleData = {
+    'id': '1',
+    'title': 'Rails is a Melting Pot',
+    'tagIds': ['2', '3']
+  };
+
+  it('should convert jsonapi data to simple', function (done) {
+    var result = transform.toSimple(apiData);
+    expect(result).to.eql(simpleData);
+    done();
+  });
+  it('should convert simple data to jsonapi', function (done) {
+    var result = transform.toJsonApi(simpleData, info);
+    expect(result).to.eql(apiData);
+    done();
+  });
+});
+
+describe('Compound documents', function () {
+  var info = {
+    'type': 'articles',
+    'relates': {
+      'author': 'people'
+    }
+  };
+  var apiData = {
+    'data': [
+      {
+        'type': 'articles',
+        'id': '1',
+        'title': 'JSON API paints my bikeshed!',
+        'links': {
+          'self': 'http://example.com/articles/1',
+          'author': {
+            'self': 'http://example.com/articles/1/links/author',
+            'resource': 'http://example.com/articles/1/author',
+            'type': 'people',
+            'id': '9'
+          },
+          'comments': {
+            'self': 'http://example.com/articles/1/links/comments',
+            'resource': 'http://example.com/articles/1/comments',
+            'type': 'comments',
+            'ids': ['5', '12']
+          }
+        }
+      }
+    ],
+    'linked': [
+      {
+        'type': 'people',
+        'id': '9',
+        'first-name': 'Dan',
+        'last-name': 'Gebhardt',
+        'twitter': 'dgeb',
+        'links': {
+          'self': 'http://example.com/people/9'
+        }
+      },
+      {
+        'type': 'comments',
+        'id': '5',
+        'body': 'First!',
+        'links': {
+          'self': 'http://example.com/comments/5'
+        }
+      },
+      {
+        'type': 'comments',
+        'id': '12',
+        'body': 'I like XML better',
+        'links': {
+          'self': 'http://example.com/comments/12'
+        }
+      }
+    ]
+  };
+  var simpleData = {
+    'articles': [
+      {
+        'id': '1',
+        'title': 'JSON API paints my bikeshed!',
+        'authorId': '9',
+        'commentsIds': ['5', '12']
+      }
+    ],
+    'people': [
+      {
+        'id': '9',
+        'first-name': 'Dan',
+        'last-name': 'Gebhardt',
+        'twitter': 'dgeb'
+      }
+    ],
+    'comments': [
+      {
+        'id': '5',
+        'body': 'First!'
+      },
+      {
+        'id': '12',
+        'body': 'I like XML better'
+      }
+    ]
+  };
+
+  it('should convert jsonapi data to simple', function (done) {
+    var result = transform.toSimple(apiData);
+    expect(result).to.eql(simpleData);
+    done();
+  });
+  it('should convert simple data to jsonapi', function (done) {
+    var result = transform.toJsonApi(simpleData, info);
+    expect(result).to.eql(apiData);
+    done();
+  });
+});
